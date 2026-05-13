ee7681fac0c018b4bbe13646ee324b8cc9fc719f
Grigory Konovalov <grigory.konovalov@forsta.com>
2026-05-13 15:09:57 +0200

    Small fix in pass store

--- PassStore/PassStore/Credential.cs
+++ PassStore/PassStore/Credential.cs
@@ -12,17 +12,17 @@ namespace PassStore
 
         public Credential(string[] oneRow)
         {
-            Name = oneRow[0];
-            Login = oneRow[1];
-            Password = oneRow[2];
+            Name = oneRow.Length > 0 ? oneRow[0] : string.Empty;
+            Login = oneRow.Length > 1 ? oneRow[1] : string.Empty;
+            Password = oneRow.Length > 2 ? oneRow[2] : string.Empty;
         }
 
         public Credential(string registryInfo)
         {
             string[] cells = registryInfo.Split(new[] { '\t' }, StringSplitOptions.RemoveEmptyEntries);
 
-            Name = cells[0];
-            Login = cells[1];
+            Name = cells.Length > 0 ? cells[0] : string.Empty;
+            Login = cells.Length > 1 ? cells[1] : string.Empty;
             Password = cells.Length > 2 ? cells[2] : string.Empty;
         }
     }
